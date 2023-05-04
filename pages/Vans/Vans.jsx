bd6e68a3741439d89eb7f4d35fb475a7126df96d
selahjs <sjemal99@gmail.com>
2023-05-04 17:31:07 +0300

    added query-params feature to vans.jsx

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,23 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    
+    const typeFilter = searchParams.get("type")
+    
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
+    
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
 
-    const vanElements = vans.map(van => (
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
@@ -22,9 +30,29 @@ export default function Vans() {
         </div>
     ))
 
+
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <Link 
+                    to="?type=simple"
+                    className="van-type simple"
+                >Simple</Link>
+                <Link 
+                    to="?type=luxury"
+                    className="van-type luxury"
+                >Luxury</Link>
+                <Link 
+                    to="?type=rugged"
+                    className="van-type rugged"
+                >Rugged</Link>
+                <Link 
+                    to="."
+                    className="van-type clear-filters"
+                >Clear filter</Link>
+            
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
